40a3c4717616d2f2e4f041d5a8d3f0d2421ac36d
Andrei Bondarev <andrei.bondarev13@gmail.com>
2024-09-13 03:32:30 +0200

    Get rid of intentional usps error

--- tools/shipping_service.rb
+++ tools/shipping_service.rb
@@ -18,8 +18,6 @@ class ShippingService
   def create_shipping_label(customer_id:, address:, provider:)
     Langchain.logger.info("[ 🚚 ] Creating shipping label for customer ID: #{customer_id}", for: self.class)
 
-    return "Invalid shipping provider. Only USPS is supported." unless ["usps"].include?(provider)
-
     {success: true, tracking_number: SecureRandom.uuid, provider: provider}
   end
 
